9fc8ddec952803977dadbc17e285553127c31e0c
Fachreza Maulana <zzafachreza@gmail.com>
2023-07-12 08:12:15 +0700

    final

---
+++ src/pages/Riwayat/index.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Riwayat() {
+    return (
+        <View>
+            <Text>Riwayat</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
